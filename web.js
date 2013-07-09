25a152bdddb1b7ffdddfc7f3c8b7a2d85c15b956
John Smith <example@stanford.edu>
2013-07-09 23:08:25 +0100

    Testing changes

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var buffer = new Buffer("Hello World Buffer", "utf-8");
   //fs.readFileSync('index.html');
-  //response.send('Hello World 2!');
-  response.send(buffer.toString('utf-8');
+  response.send('Hello World 2!');
+  //response.send(buffer.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
