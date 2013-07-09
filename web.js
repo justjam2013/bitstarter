968a1dca2b15a156209e7487065d501b487dc50b
John Smith <example@stanford.edu>
2013-07-09 23:34:07 +0100

    Read index.html file

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('index.html');
-  response.send(buffer.toString('utf-8'));
+  var indexBuffer = fs.readFileSync('index.html');
+  response.send(indexBuffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
